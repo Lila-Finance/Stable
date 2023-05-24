b3c075ce17dd2b56b30356eef3aa6ac7d0091eb9
Ram Potham <ram.potham@gmail.com>
2023-05-23 21:09:34 -0400

    merge

--- frontend/src/components/PoolManagement.js
+++ frontend/src/components/PoolManagement.js
@@ -5,13 +5,13 @@ import Faucet from "./Faucet";
 import AccessTimeFilledIcon from "@mui/icons-material/AccessTimeFilled";
 import MonetizationOnIcon from "@mui/icons-material/MonetizationOn";
 import { tokenContract } from "./Provider";
-import React, {useState} from "react";
+import React, { useState } from "react";
 import { Box, Button, Typography, Grid, ButtonBase } from "@mui/material";
 import { poolDeployerContract } from "./Provider";
-import KeyboardArrowLeftIcon from '@mui/icons-material/KeyboardArrowLeft';
-import KeyboardArrowRightIcon from '@mui/icons-material/KeyboardArrowRight';
-import RefreshIcon from '@mui/icons-material/Refresh';
-import LockIcon from '@mui/icons-material/Lock';
+import KeyboardArrowLeftIcon from "@mui/icons-material/KeyboardArrowLeft";
+import KeyboardArrowRightIcon from "@mui/icons-material/KeyboardArrowRight";
+import RefreshIcon from "@mui/icons-material/Refresh";
+import LockIcon from "@mui/icons-material/Lock";
 import CircularProgress from "@mui/material/CircularProgress";
 
 function PoolManagement({
@@ -40,67 +40,82 @@ function PoolManagement({
 
   return (
     <div>
-      <Box
-        className="mb-4"
-        sx={{ borderRadius: 1 }}
-      >
+      <Box className="mb-4" sx={{ borderRadius: 1 }}>
         {numPools > 0 && (
           <>
-          <Box sx={{ display: 'flex', justifyContent: 'space-between', marginBottom: 1 }}>
-          <Button
-            variant="outlined"
-            color="secondary"
-            onClick={async () => {
-              if (poolNum > 0) {
-                setLoadingPrev(true);
-                setIsLoading(true);
-                setPoolNum(poolNum - 1);
-                setTimeout(() => {
-                  setLoadingPrev(false);
-                  setIsLoading(false);
-                }, 1000);
-              }
-            }}
-            sx={{ marginRight: 1 }}
-            startIcon={loadingPrev ? <CircularProgress size={24} /> : <KeyboardArrowLeftIcon />}
-          >
-            Prev Pool
-          </Button>
-
-            <Typography color="secondary" align="center" variant="h6">
-              Pool {poolNum}
-            </Typography>
-            
-            <Button
-              variant="outlined"
-              color="secondary"
-              onClick={async () => {
-                if (poolNum < numPools) {
-                  setLoadingNext(true);
-                  setIsLoading(true);
-                  setPoolNum(poolNum + 1);
-                  setTimeout(() => {
-                    setLoadingNext(false);
-                    setIsLoading(false);
-                  }, 1000);
-                }
+            <Box
+              sx={{
+                display: "flex",
+                justifyContent: "space-between",
+                marginBottom: 1,
               }}
-              sx={{ marginRight: 1 }}
-              endIcon={loadingNext ? <CircularProgress size={24} /> : <KeyboardArrowRightIcon />}
             >
-              Next Pool
-            </Button>
+              <Button
+                variant="outlined"
+                color="secondary"
+                onClick={async () => {
+                  if (poolNum > 0) {
+                    setLoadingPrev(true);
+                    setIsLoading(true);
+                    setPoolNum(poolNum - 1);
+                    setTimeout(() => {
+                      setLoadingPrev(false);
+                      setIsLoading(false);
+                    }, 1000);
+                  }
+                }}
+                sx={{ marginRight: 1 }}
+                startIcon={
+                  loadingPrev ? (
+                    <CircularProgress size={24} />
+                  ) : (
+                    <KeyboardArrowLeftIcon />
+                  )
+                }
+              >
+                Prev Pool
+              </Button>
+
+              <Typography color="secondary" align="center" variant="h6">
+                Pool {poolNum}
+              </Typography>
+
+              <Button
+                variant="outlined"
+                color="secondary"
+                onClick={async () => {
+                  if (poolNum < numPools - 1) {
+                    setLoadingNext(true);
+                    setIsLoading(true);
+                    setPoolNum(poolNum + 1);
+                    setTimeout(() => {
+                      setLoadingNext(false);
+                      setIsLoading(false);
+                    }, 1000);
+                  }
+                }}
+                sx={{ marginRight: 1 }}
+                endIcon={
+                  loadingNext ? (
+                    <CircularProgress size={24} />
+                  ) : (
+                    <KeyboardArrowRightIcon />
+                  )
+                }
+              >
+                Next Pool
+              </Button>
             </Box>
           </>
         )}
       </Box>
-      <Box 
+      <Box
         className="mb-3"
         sx={{
           // backgroundColor: '#40386b',
-          border: '1px solid #40386b',
-          borderRadius: '4px',
-          padding: '20px',
+          border: "1px solid #40386b",
+          borderRadius: "4px",
+          padding: "20px",
         }}
       >
         <Box display="flex" alignItems="center">
@@ -132,7 +147,9 @@ function PoolManagement({
               setLoadingRefresh(false);
             }, 1000);
           }}
-          startIcon={loadingRefresh ? <CircularProgress size={24} /> : <RefreshIcon />}
+          startIcon={
+            loadingRefresh ? <CircularProgress size={24} /> : <RefreshIcon />
+          }
         >
           Refresh Contracts
         </Button>
