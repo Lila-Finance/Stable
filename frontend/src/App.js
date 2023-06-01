fcc5000f105ec2f7faf927ec383ba965bac072f1
lynchee-owo <lynchee.owo@gmail.com>
2023-05-31 20:10:19 -0700

    update pool stats

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,48 +1,22 @@
 import React, { useState, useEffect } from "react";
 import { ethers } from "ethers";
-import FixedNFTs from "./components/FixedNFTs";
-import SupplyFixed from "./components/SupplyFixed";
-import FixedNFTData from "./components/FixedNFTData";
-import { poolDeployerContract } from "./components/Provider";
-import SupplyVariable from "./components/SupplyVariable";
-import VariableNFTs from "./components/VariableNFTs";
-import VariableNFTData from "./components/VariableNFTData";
 import NavBar from "./components/NavBar";
-import CreatePool from "./components/CreatePool";
-import DeletePool from "./components/DeletePool";
-import FastForward from "./components/FastForward";
-import PoolManagement from "./components/PoolManagement";
 import PoolCard from "./components/PoolCard";
+import FixedNFTs from "./components/FixedNFTs";
+import VariableNFTs from "./components/VariableNFTs";
+import PoolManagement from "./components/PoolManagement";
 import {
-  Box,
-  Button,
   Container,
   Grid,
-  Typography,
   CssBaseline,
   createTheme,
   ThemeProvider,
-  Card,
-  CardContent,
-  Link,
 } from "@mui/material";
-import AccountBalanceWalletIcon from "@mui/icons-material/AccountBalanceWallet";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import "./App.css";
-import FixedNFTAbi from "./abi/FixedNFT.json";
-import VariableNFTAbi from "./abi/VariableNFT.json";
-import PoolAbi from "./abi/Pool.json";
 import { useAccount } from "wagmi";
 import Alert from "@mui/material/Alert";
 import AlertTitle from "@mui/material/AlertTitle";
-import CircularProgress from "@mui/material/CircularProgress";
 
-const poolAbi = PoolAbi.abi;
-const fixedNFTAbi = FixedNFTAbi.abi;
-const variableNFTAbi = VariableNFTAbi.abi;
-
-const provider = new ethers.providers.Web3Provider(window.ethereum);
-const signer = provider.getSigner();
 
 const theme = createTheme({
   palette: {
@@ -58,75 +32,10 @@ const theme = createTheme({
   },
 });
 
-/*const FixedNFT = await ethers.getContractFactory("FixedNFT");
-const VariableNFT = await ethers.getContractFactory("VariableNFT");
-const Pool = await ethers.getContractFactory("Pool");*/
-
 function App() {
-  const [fixedRate, setFixedRate] = useState(ethers.BigNumber.from("0"));
-  const [variableRate, setVariableRate] = useState(ethers.BigNumber.from("0"));
-  const [timeSinceStart, setTimeSinceStart] = useState(0);
-  const [lockDuration, setLockDuration] = useState(0);
-  const [poolContract, setPoolContract] = useState(null);
-  const [fixedNFTContract, setFixedNFTContract] = useState(null);
-  const [variableNFTContract, setVariableNFTContract] = useState(null);
   const [numPools, setNumPools] = useState(2);
-  const [poolNum, setPoolNum] = useState(0);
-  const [refreshKey, setRefreshKey] = useState(0);
   const [developerMode, setDeveloperMode] = useState(false);
-  const { address, isConnecting, isDisconnected } = useAccount();
-  const [loading, setLoading] = useState(false);
-
-  const handleRefresh = () => {
-    setRefreshKey((prevKey) => prevKey + 1);
-  };
-
-  //effect when address changes
-  useEffect(() => {
-    async function setPool() {
-      if (numPools > 0) {
-        const poolInfo = await poolDeployerContract.pools(poolNum % numPools);
-        const poolContract = new ethers.Contract(poolInfo, poolAbi, signer);
-        const fixedNFTContract = new ethers.Contract(
-          await poolContract.fixedNFT(),
-          fixedNFTAbi,
-          signer
-        );
-        const variableNFTContract = new ethers.Contract(
-          await poolContract.variableNFT(),
-          variableNFTAbi,
-          signer
-        );
-        setPoolContract(poolContract);
-        setFixedNFTContract(fixedNFTContract);
-        setVariableNFTContract(variableNFTContract);
-      }
-      //const pools = await poolDeployerContract.getPools();
-    }
-    if (address) {
-      setPool();
-    }
-  }, [poolNum, address]);
-
-  useEffect(() => {
-    async function setRate() {
-      const rates = await poolContract.interests();
-      const lockDuration = await poolContract.lockDuration();
-      const secondsInADay = 86400;
-      const lockDurationInDays = lockDuration.div(secondsInADay);
-
-      setLockDuration(`${lockDurationInDays.toString()} days`);
-      const timeSinceStart = await poolContract.timeSinceStart();
-      const timeSinceStartInDays = timeSinceStart.div(secondsInADay);
-      setTimeSinceStart(`${timeSinceStartInDays.toString()} days`);
-
-      setFixedRate(rates[0]);
-      setVariableRate(rates[1]);
-    }
-    if (poolContract) {
-      setRate();
-    }
-  }, [poolContract, refreshKey]);
+  const { address } = useAccount();
 
   return (
     <ThemeProvider theme={theme}>
@@ -155,118 +64,50 @@ function App() {
       </Alert>
       <NavBar onDeveloperModeChange={setDeveloperMode} />
       <Container>
-        <Grid container spacing={7} justifyContent="center" alignItems="center" mt={4}>
-          <Grid item xs={4}>
-            <PoolCard status={"expired"} address={address} poolContract={poolContract}/>
-          </Grid>
-          <Grid item xs={4}>
-            <PoolCard status={"inprogress"} address={address} poolContract={poolContract}/>
-          </Grid>
-          <Grid item xs={4}>
-            <PoolCard status={"done"} address={address} poolContract={poolContract}/>
-          </Grid>
-        </Grid>
-        {/* {poolContract ? (
-          <Box position="relative">
-            <PoolManagement
-              poolContract={poolContract}
-              timeSinceStart={timeSinceStart}
-              lockDuration={lockDuration}
-              numPools={numPools}
-              setPoolNum={setPoolNum}
-              poolNum={poolNum}
-              handleRefresh={handleRefresh}
-              setIsLoading={setLoading}
-            />
-            <Grid container spacing={5}>
-              <Grid item xs={6}>
-                <FixedNFTData
-                  poolContract={poolContract}
-                  rate={fixedRate}
-                  refreshKey={refreshKey}
-                />
-                <Card className="mb-4" sx={{ backgroundColor: "#40386b" }}>
-                  <CardContent>
-                    <SupplyFixed
-                      address={address}
-                      poolContract={poolContract}
-                    />
-                  </CardContent>
-                </Card>
-                <Card className="mb-4" sx={{ backgroundColor: "#40386b" }}>
-                  <CardContent>
-                    <FixedNFTs
-                      address={address}
-                      rate={fixedRate}
-                      fixedNFTContract={fixedNFTContract}
-                      poolContract={poolContract}
-                      refreshKey={refreshKey}
-                    />
-                  </CardContent>
-                </Card>
-              </Grid>
-              <Grid item xs={6}>
-                <VariableNFTData
-                  poolContract={poolContract}
-                  rate={variableRate}
-                  refreshKey={refreshKey}
-                />
-                <Card className="mb-4" sx={{ backgroundColor: "#40386b" }}>
-                  <CardContent>
-                    <SupplyVariable
-                      address={address}
-                      poolContract={poolContract}
-                    />
-                  </CardContent>
-                </Card>
-                <Card className="mb-4" sx={{ backgroundColor: "#40386b" }}>
-                  <CardContent>
-                    <VariableNFTs
-                      address={address}
-                      rate={variableRate}
-                      variableNFTContract={variableNFTContract}
-                      poolContract={poolContract}
-                      refreshKey={refreshKey}
-                    />
-                  </CardContent>
-                </Card>
-              </Grid>
+          <Grid container spacing={7} justifyContent="center" alignItems="center" mt={4}>
+            <Grid item xs={4}>
+              <PoolCard 
+                status={"expired"} 
+                address={address}
+                numPools={numPools}
+                poolNum={0}
+              />
             </Grid>
-            {loading && (
-              <Box
-                position="absolute"
-                top={0}
-                right={0}
-                bottom={0}
-                left={0}
-                bgcolor="rgba(0,0,0,0.5)"
-                display="flex"
-                justifyContent="center"
-                alignItems="center"
-              >
-                <CircularProgress color="primary" />
-              </Box>
-            )}
-          </Box>
-        ) : (
-          <Container>
-            <Box mt={10} mb={6}>
-              <Typography
-                variant="h4"
-                component="div"
-                align="center"
-                sx={{
-                  fontWeight: "bold",
-                  color: "primary.main",
-                  marginTop: 2,
-                  marginBottom: 2,
-                }}
-              >
-                Interest Rate Swap
-              </Typography>
-            </Box>
-          </Container>
-        )} */}
+            <Grid item xs={4}>
+              <PoolCard 
+                status={"inprogress"} 
+                address={address}
+                numPools={numPools}
+                poolNum={1}
+              />
+            </Grid>
+            <Grid item xs={4}>
+              <PoolCard 
+                status={"done"} 
+                address={address}
+                numPools={numPools}
+                poolNum={2}
+              />
+            </Grid>
+          </Grid>
+        {/* 
+            <Grid container spacing={5}>
+              <FixedNFTs
+                address={address}
+                rate={fixedRate}
+                fixedNFTContract={fixedNFTContract}
+                poolContract={poolContract}
+                refreshKey={refreshKey}
+              />
+              <VariableNFTs
+                address={address}
+                rate={variableRate}
+                variableNFTContract={variableNFTContract}
+                poolContract={poolContract}
+                refreshKey={refreshKey}
+              />
+            </Grid>
+        )*/}
       </Container>
     </ThemeProvider>
   );
--- frontend/src/components/PoolCard.js
+++ frontend/src/components/PoolCard.js
@@ -19,13 +19,22 @@ import {
 import redEllipse from '../images/red_ellipse.png';
 import yellowEllipse from '../images/yellow_ellipse.png';
 import greenEllipse from '../images/green_ellipse.png';
-import { approveSpend, sendParams } from "./Provider";
 import { ethers } from "ethers";
+import { approveSpend, sendParams } from "./Provider";
+import { poolDeployerContract } from "./Provider";
+import PoolAbi from "../abi/Pool.json";
+import FixedNFTAbi from "../abi/FixedNFT.json";
+import VariableNFTAbi from "../abi/VariableNFT.json";
 
-const PoolCard = ({ status, address, poolContract }) => {
+const poolAbi = PoolAbi.abi;
+const fixedNFTAbi = FixedNFTAbi.abi;
+const variableNFTAbi = VariableNFTAbi.abi;
+
+const provider = new ethers.providers.Web3Provider(window.ethereum);
+const signer = provider.getSigner();
+
+const PoolCard = ({ status, address, numPools, poolNum }) => {
   const [poolStatus, setPoolStatus] = useState(null);
-  // TODO: change days
-  const [days, setDays] = useState(7);
   // State to handle "Fix" and "Variable" toggle button
   const [supplyType, setSupplyType] = useState('Variable');
   // state from supplyFixed.js
@@ -35,6 +44,84 @@ const PoolCard = ({ status, address, poolContract }) => {
   const [error, setError] = useState(null);
   // state from supplyVariable.js
   const [varMax, setVarMax] = useState(null);
+  // state from FixedNFTData.js
+  const [fixedSupply, setFixedSupply] = useState(0);
+  const [fixedLimit, setFixedLimit] = useState(0);
+  // state from VariableNFTData.js
+  const [variableSupply, setVariableSupply] = useState(0);
+  const [variableLimit, setVariableLimit] = useState(0);
+  // state from App.js
+  const [poolContract, setPoolContract] = useState(null);
+  const [fixedNFTContract, setFixedNFTContract] = useState(null);
+  const [variableNFTContract, setVariableNFTContract] = useState(null);
+  const [lockDuration, setLockDuration] = useState(0);
+  const [timeSinceStart, setTimeSinceStart] = useState(0);
+  const [fixedRate, setFixedRate] = useState(ethers.BigNumber.from("0"));
+  const [variableRate, setVariableRate] = useState(ethers.BigNumber.from("0"));
+
+  // function from App.js
+  useEffect(() => {
+    async function setPool() {
+      if (numPools > 0) {
+        const poolInfo = await poolDeployerContract.pools(poolNum % numPools);
+        const poolContract = new ethers.Contract(poolInfo, poolAbi, signer);
+        const fixedNFTContract = new ethers.Contract(
+          await poolContract.fixedNFT(),
+          fixedNFTAbi,
+          signer
+        );
+        const variableNFTContract = new ethers.Contract(
+          await poolContract.variableNFT(),
+          variableNFTAbi,
+          signer
+        );
+        setPoolContract(poolContract);
+        setFixedNFTContract(fixedNFTContract);
+        setVariableNFTContract(variableNFTContract);
+      }
+      //const pools = await poolDeployerContract.getPools();
+    }
+    if (address) {
+      setPool();
+    }
+  }, [poolNum, address, numPools]);
+
+  // function from App.js 
+  useEffect(() => {
+    async function setRate() {
+      const rates = await poolContract.interests();
+      const lockDuration = await poolContract.lockDuration();
+      const secondsInADay = 86400;
+      const lockDurationInDays = lockDuration.div(secondsInADay);
+
+      setLockDuration(`${lockDurationInDays.toString()}D`);
+      const timeSinceStart = await poolContract.timeSinceStart();
+      const timeSinceStartInDays = timeSinceStart.div(secondsInADay);
+      setTimeSinceStart(`${timeSinceStartInDays.toString()} days`);
+
+      setFixedRate(rates[0]);
+      setVariableRate(rates[1]);
+    }
+    if (poolContract) {
+      setRate();
+    }
+  }, [poolContract]);
+
+  // function from FixedNFTData.js
+  useEffect(() => {
+    async function fetchData() {
+      let fixedSupply = await poolContract.totalDepositedFixed();
+      setFixedSupply(fixedSupply);
+      let limitFixed = await poolContract.fixedPoolLimit();
+      setFixedLimit(limitFixed);
+
+      let variableSupply = await poolContract.totalDepositedVariable();
+      setVariableSupply(variableSupply);
+      let limitVariable = await poolContract.variablePoolLimit();
+      setVariableLimit(limitVariable);
+    }
+    fetchData();
+  }, [poolContract]);
 
   // function from supplyFixed.js
   const handleFixMaxClick = async () => {
@@ -141,68 +228,95 @@ const PoolCard = ({ status, address, poolContract }) => {
     }
   }, [status]);
 
-const statusTable = () => {
-  const emptyRow = (
-    <TableRow>
-      <TableCell size="small" component="th" scope="row" style={{ color: "#9C9CA6", border: 'none' }}>P</TableCell>
-    </TableRow>
-  );
+  const poolDataTable = () => (
+    <Table>
+      <TableBody>
+        <TableRow>
+          <TableCell size="small" component="th" scope="row" style={{ color: "#9C9CA6", border: 'none' }}> </TableCell>
+          <TableCell size="small" align="right" style={{ color: "#9C9CA6", border: 'none' }}>Fixed</TableCell>
+          <TableCell size="small" align="right" style={{ color: "#9C9CA6", border: 'none' }}>Variable</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell size="small" component="th" scope="row" style={{ color: "#9C9CA6", border: 'none' }}>APR</TableCell>
+          <TableCell size="small" align="right" style={{ color: "#4C4C51", border: 'none' }}>{ethers.utils.formatEther(fixedRate)}%</TableCell>
+          <TableCell size="small" align="right" style={{ color: "#4C4C51", border: 'none' }}>{ethers.utils.formatEther(variableRate)}%</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell size="small" component="th" scope="row" style={{ color: "#9C9CA6", border: 'none' }}>Cap</TableCell>
+          <TableCell size="small" align="right" style={{ color: "#4C4C51", border: 'none' }}>{ethers.utils.formatEther(fixedLimit)} DAI</TableCell>
+          <TableCell size="small" align="right" style={{ color: "#4C4C51", border: 'none' }}>{ethers.utils.formatEther(variableLimit)} DAI</TableCell>
+        </TableRow>
+        <TableRow>
+          <TableCell size="small" component="th" scope="row" style={{ color: "#9C9CA6", border: 'none' }}>Deposits</TableCell>
+          <TableCell size="small" align="right" style={{ color: "#4C4C51", border: 'none' }}>{ethers.utils.formatEther(fixedSupply)} DAI</TableCell>
+          <TableCell size="small" align="right" style={{ color: "#4C4C51", border: 'none' }}>{ethers.utils.formatEther(variableSupply)} DAI</TableCell>
+        </TableRow>
+      </TableBody>
+    </Table>
+  )
 
-  if (status === 'expired') {
-    return (
-      <Table>
-        <TableBody>
-          <TableRow>
-            <TableCell size="small" component="th" scope="row" style={{ color: "#9C9CA6", border: 'none' }}>Expired</TableCell>
-          </TableRow>
-          {emptyRow}
-        </TableBody>
-      </Table>
+  const statusTable = () => {
+    const emptyRow = (
+      <TableRow>
+        <TableCell size="small" component="th" scope="row" style={{ color: "#9C9CA6", border: 'none' }}></TableCell>
+      </TableRow>
     );
-  } else if (status === 'inprogress') {
-    return (
-      <Table>
-        <TableBody>
-          <TableRow>
-            <TableCell size="small" component="th" scope="row" style={{ color: "#9C9CA6", border: 'none' }}>Expiry</TableCell>
-            <TableCell size="small" align="right" style={{ color: "#4C4C51", border: 'none' }}>xxx. xx,xx</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell size="small" component="th" scope="row" style={{ color: "#9C9CA6", border: 'none' }}>Payout</TableCell>
-            <TableCell size="small" align="right" style={{ color: "#4C4C51", border: 'none' }}>0 of x</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    );
-  } else if (status === 'done') {
-    return (
-      <Table>
-        <TableBody>
-          <TableRow>
-            <TableCell size="small" component="th" scope="row" style={{ color: "#9C9CA6", border: 'none' }}>Maturity</TableCell>
-            <TableCell size="small" align="right" style={{ color: "#4C4C51", border: 'none' }}>xxx. xx,xx</TableCell>
-          </TableRow>
-          <TableRow>
-            <TableCell size="small" component="th" scope="row" style={{ color: "#9C9CA6", border: 'none' }}>Payout</TableCell>
-            <TableCell size="small" align="right" style={{ color: "#4C4C51", border: 'none' }}>x of x+1</TableCell>
-          </TableRow>
-        </TableBody>
-      </Table>
-    );
-  } else {
-    return (
-      <Table>
-        <TableBody>
-          {emptyRow}
-          {emptyRow}
-        </TableBody>
-      </Table>
-    )
+
+    if (status === 'expired') {
+      return (
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell size="small" component="th" scope="row" style={{ color: "#9C9CA6", border: 'none' }}>Expired</TableCell>
+            </TableRow>
+            {emptyRow}
+          </TableBody>
+        </Table>
+      );
+    } else if (status === 'inprogress') {
+      return (
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell size="small" component="th" scope="row" style={{ color: "#9C9CA6", border: 'none' }}>Expiry</TableCell>
+              <TableCell size="small" align="right" style={{ color: "#4C4C51", border: 'none' }}>xxx. xx,xx</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell size="small" component="th" scope="row" style={{ color: "#9C9CA6", border: 'none' }}>Payout</TableCell>
+              <TableCell size="small" align="right" style={{ color: "#4C4C51", border: 'none' }}>0 of x</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      );
+    } else if (status === 'done') {
+      return (
+        <Table>
+          <TableBody>
+            <TableRow>
+              <TableCell size="small" component="th" scope="row" style={{ color: "#9C9CA6", border: 'none' }}>Maturity</TableCell>
+              <TableCell size="small" align="right" style={{ color: "#4C4C51", border: 'none' }}>xxx. xx,xx</TableCell>
+            </TableRow>
+            <TableRow>
+              <TableCell size="small" component="th" scope="row" style={{ color: "#9C9CA6", border: 'none' }}>Payout</TableCell>
+              <TableCell size="small" align="right" style={{ color: "#4C4C51", border: 'none' }}>x of x+1</TableCell>
+            </TableRow>
+          </TableBody>
+        </Table>
+      );
+    } else {
+      return (
+        <Table>
+          <TableBody>
+            {emptyRow}
+            {emptyRow}
+          </TableBody>
+        </Table>
+      )
+    }
   }
-}
 
   return (
-  <Card variant="outlined" sx={{ borderColor: '#FBFBEC', backgroundColor: '#ECECFB', borderRadius: '10px' }}>
+  <Card variant="outlined" sx={{ borderColor: '#FBFBEC', backgroundColor: '#ECECFB', borderRadius: '10px', height:'100%' }}>
       <CardContent>
         <Typography 
           variant="h6" 
@@ -216,35 +330,12 @@ const statusTable = () => {
           }}
         >
           <div style={{ flexGrow: 1, textAlign: 'center' }}>
-            DAI (Aave) <strong>{`${days}d`}</strong>
+            DAI (Aave) <strong>{lockDuration}</strong>
           </div>
           {poolStatus && <img src={poolStatus} alt="status ellipse" style={{ width: '10%', height: '10%', marginLeft: 'auto' }}/>}
         </Typography>
         <Divider sx={{ bgcolor: '#FBFBEC' }} />
-        <Table>
-          <TableBody>
-            <TableRow>
-              <TableCell size="small" component="th" scope="row" style={{ color: "#9C9CA6", border: 'none' }}> </TableCell>
-              <TableCell size="small" align="right" style={{ color: "#9C9CA6", border: 'none' }}>Fixed</TableCell>
-              <TableCell size="small" align="right" style={{ color: "#9C9CA6", border: 'none' }}>Variable</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell size="small" component="th" scope="row" style={{ color: "#9C9CA6", border: 'none' }}>APR</TableCell>
-              <TableCell size="small" align="right" style={{ color: "#4C4C51", border: 'none' }}>2.50%</TableCell>
-              <TableCell size="small" align="right" style={{ color: "#4C4C51", border: 'none' }}>3.00%</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell size="small" component="th" scope="row" style={{ color: "#9C9CA6", border: 'none' }}>Cap</TableCell>
-              <TableCell size="small" align="right" style={{ color: "#4C4C51", border: 'none' }}>50,000</TableCell>
-              <TableCell size="small" align="right" style={{ color: "#4C4C51", border: 'none' }}>1,250</TableCell>
-            </TableRow>
-            <TableRow>
-              <TableCell size="small" component="th" scope="row" style={{ color: "#9C9CA6", border: 'none' }}>Deposits</TableCell>
-              <TableCell size="small" align="right" style={{ color: "#4C4C51", border: 'none' }}>xx,xxx</TableCell>
-              <TableCell size="small" align="right" style={{ color: "#4C4C51", border: 'none' }}>x,xxx</TableCell>
-            </TableRow>
-          </TableBody>
-        </Table>
+        {poolDataTable()}
         <Divider sx={{ bgcolor: '#FBFBEC', marginTop: '10px', marginBottom: '10px' }} />
         {statusTable()}
         <Divider sx={{ bgcolor: '#FBFBEC', marginTop: '10px', marginBottom: '10px' }} />
