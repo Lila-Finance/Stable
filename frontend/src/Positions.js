ad3c365bd29c07a4fd513773a64b336ad71ca637
marlonedwards <94414329+marlonedwards@users.noreply.github.com>
2023-06-14 13:38:05 -0400

    Wrap positions in conditional
    Only display when positions are active

--- frontend/src/Positions.js
+++ frontend/src/Positions.js
@@ -7,14 +7,43 @@ import { useAccount } from "wagmi";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { TASK_COMPILE_SOLIDITY_LOG_DOWNLOAD_COMPILER_END } from "hardhat/builtin-tasks/task-names";
 
-function Positions() {
-    /*const [numPools, setNumPools] = useState(2);
-    const [developerMode, setDeveloperMode] = useState(false);
-    const { address } = useAccount();*/
-        return (
-            <h1>Positions</h1>
-            
-        );
-}
+const theme = createTheme({
+    palette: {
+      primary: {
+        main: "#9c88ff",
+      },
+      secondary: {
+        main: "#e6d7ff",
+      },
+      background: {
+        default: "#FFFDFF",
+      },
+    },
+  });
+
+  const [numPools, setNumPools] = useState(2);
+  const [developerMode, setDeveloperMode] = useState(false);
+  const { address } = useAccount();
+  const Positions = ({ positions }) => {
+    return (
+      <Container>
+        {positions.length > 0 && (
+          <Grid container spacing={7} justifyContent="center" alignItems="center" mt={4}>
+            {positions.map((position, index) => (
+              <Grid item xs={12} md={4} key={index}>
+                <PositionCard
+                  status={position.status}
+                  address={position.address}
+                  numPools={position.numPools}
+                  poolNum={position.poolNum}
+                />
+              </Grid>
+            ))}
+          </Grid>
+        )}
+      </Container>
+    );
+
+  };
 
 export default Positions;
